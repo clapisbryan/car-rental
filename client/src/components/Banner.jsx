afb04307eddccca5568816cf1d27b5e6aacbaa08
bryan-github-01 <bcalapis@fydesigns.ph>
2025-07-06 19:51:34 +0800

    Created Car Details

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Banner = () => {
     return (
         <div className='flex flex-col md:flex-row md:items-start items-center justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#0558fe] to-[#a9cfff] max-w-6xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
-            <div className='text-whtie'>
+            <div className='text-white'>
                 <h2 className='text-3xl font-medium'>Do You Own a Luxury Car?</h2>
                 <p className='mt-2'>Monetize your vehicle effortlessly by listing it on CarRental.</p>
                 <p className='max-w-130'>We take care of insurance, driver verification and secure payments -  so you can earn passive income, stress-free.</p>
